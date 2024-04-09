f84c704f89589093368e4b618e5cfc9921d1585b
Pedro Vinicios <110180491+PedroVinicioss@users.noreply.github.com>
2024-04-09 08:46:21 -0300

    Bottom Navigator

---
+++ src/screens/Cart/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Cart() {
+    return (
+        <View style={styles.container}>
+        <Text>Cart</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
